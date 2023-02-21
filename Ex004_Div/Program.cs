ad8498e362ae876ec185024c064751b363e6c0d3
iv_dr <iv_drojjin@mail.ru>
2023-02-21 23:16:48 +0300

    add Ex004_Div

---
+++ Ex004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!!!");
+double numberA = 2;
+double numberB = 5;
+double div = numberA / numberB;
+Console.Write("Div : ");
+Console.WriteLine(div);
