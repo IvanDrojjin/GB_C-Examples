3d424a76eacaed81339b3068fa31a6610024148e
iv_dr <iv_drojjin@mail.ru>
2023-02-21 23:00:07 +0300

    add commit Ex003_Sum0

---
+++ Ex003_Sum0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!!!");
+int numberA = 2;
+int numberB = 5;
+int sum = numberA + numberB;
+Console.Write("Sum : ");
+Console.WriteLine(sum);
