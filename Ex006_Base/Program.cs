ab23e29eea7260fa20c107bed629cb3fca581dfe
iv_dr <iv_drojjin@mail.ru>
2023-02-21 23:52:09 +0300

    add Ex006_Base  IF THEN ELSE

---
+++ Ex006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 1, b = 2, c = 6, d = 8, e = 4;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+// if (a > max) max = a;
+
+
+Console.Write("Max : ");
+Console.WriteLine(max);
+
