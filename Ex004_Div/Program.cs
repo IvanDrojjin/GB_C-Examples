4d68c1feb2c9ff09398101c43bc13b04e45cc6bf
iv_dr <iv_drojjin@mail.ru>
2023-02-21 23:28:43 +0300

    add new Random().Next(1, 10) Ex004_Div

--- Ex004_Div/Program.cs
+++ Ex004_Div/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!!!");
-double numberA = 2;
-double numberB = 5;
+double numberA = new Random().Next(1, 10);
+double numberB = new Random().Next(1, 10);
 double div = numberA / numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.Write("Div : ");
 Console.WriteLine(div);
